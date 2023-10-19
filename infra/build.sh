b4697170e443c1a51d0a1cf5a89fdb14306c3c13
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-10-19 22:40:08 +0200

    feat(infra): use exactly the remote branch

--- infra/build.sh
+++ infra/build.sh
@@ -1,6 +1,7 @@
 echo "change detected rebuilding" ${pwd}
 cd /home/debian/monorepo
 git pull
+git reset --hard origin/main
 # TODO: put build instructions here instead of in the yaml file
 cd code
 npm install
