b19d3cb3dd0ea5e9e7cfe475811d2ee78fbc2830
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-10-27 21:17:05 +0200

    style: add husky prettier and commitlint

---
+++ code/frontend/commitlint.config.cjs
@@ -0,0 +1,9 @@
+// https://dev.to/edsonjuniornarvaes/commitizen-commitlint-3elg
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+
+    /*
+     * Custom URL to show upon failure
+     */
+    helpUrl: ' Per maggiori informazioni sui linters vedere https://github.com/Flecart/Squealer/wiki#git-commits',
+};
