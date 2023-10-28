6f2fb75163ae6b3c9d98385b570dd806f6462754
jantizio <emanuele.pischetola@studio.unibo.it>
2023-10-28 10:07:22 +0200

    style: added vite alias for absolute imports

--- code/frontend/vite.config.ts
+++ code/frontend/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+import { fileURLToPath, URL } from 'url';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // TODO: aggiungere alias
+    plugins: [react()],
+    resolve: {
+        alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
+    },
 });
