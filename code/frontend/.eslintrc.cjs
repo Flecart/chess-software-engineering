29f08da38da51841dbf03154e7700dee175fd01c
jantizio <emanuele.pischetola@studio.unibo.it>
2023-10-28 10:36:31 +0200

    style: eslint rule for feature folder

--- code/frontend/.eslintrc.cjs
+++ code/frontend/.eslintrc.cjs
@@ -7,5 +7,16 @@ module.exports = {
     plugins: ['react-refresh'],
     rules: {
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'no-restricted-imports': [
+            'error',
+            {
+                patterns: [
+                    {
+                        group: ['@/features/*/*'],
+                        message: 'Please import from the feature folder instead.',
+                    },
+                ],
+            },
+        ],
     },
 };
