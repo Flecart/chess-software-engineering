83df905d3e62fa5048bd98039f8dd5476bd49b99
jantizio <emanuele.pischetola@studio.unibo.it>
2023-11-05 11:29:08 +0100

    feat(front): polling to get board updated

--- code/frontend/src/features/chessboard/components/Chessboard.tsx
+++ code/frontend/src/features/chessboard/components/Chessboard.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Chessboard as ReactChessboard } from 'react-chessboard';
 import { generateFogObject, generateStandardFen } from '../utils/fen';
-import { makeMove } from '../api/game';
+import { getBoard, makeMove } from '../api/game';
 
 const startBlackFEN = 'rnbqkbnr/pppppppp/......../......../XXXXXXXX/XXXXXXXX/XXXXXXXX/XXXXXXXX';
 const startWhiteFEN = startBlackFEN.toUpperCase().split('/').reverse().join('/');
@@ -16,6 +16,22 @@ type Props = {
 export const Chessboard = ({ gameId, boardOrientation, style }: Props) => {
     const [fen, setFen] = useState(boardOrientation === 'white' ? startWhiteFEN : startBlackFEN);
 
+    useEffect(() => {
+        getBoard(gameId, boardOrientation).then((res) => {
+            setFen(res.board.split('\n').reverse().join('/'));
+        });
+        const pollingBoard = setInterval(() => {
+            getBoard(gameId, boardOrientation).then((res) => {
+                if (res.has_enemy_moved) setFen(res.board.split('\n').reverse().join('/'));
+            });
+        }, 1000);
+        return () => {
+            clearInterval(pollingBoard);
+        };
+        // i just need to run this once, so i can ignore the dependency array
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <div
             style={{
@@ -29,7 +45,12 @@ export const Chessboard = ({ gameId, boardOrientation, style }: Props) => {
                 boardOrientation={boardOrientation}
                 position={generateStandardFen(fen)}
                 customSquareStyles={generateFogObject(fen)}
-                onPieceDrop={(from, to) => {
+                onPieceDrop={(from, to, piece) => {
+                    if (
+                        (boardOrientation === 'black' && piece.startsWith('w')) ||
+                        (boardOrientation === 'white' && piece.startsWith('b'))
+                    )
+                        return false;
                     makeMove(gameId, `${from}${to}`).then((res) => {
                         setFen(res.board.split('\n').reverse().join('/'));
                     });
