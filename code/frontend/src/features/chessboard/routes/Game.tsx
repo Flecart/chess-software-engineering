7f2ff55b0358177463c36b457161faa78c676a2c
AlleNeri <aleneri2002@gmail.com>
2023-11-05 12:50:52 +0100

    feat(front): end Game
    Add a modal for the end game.

--- code/frontend/src/features/chessboard/routes/Game.tsx
+++ code/frontend/src/features/chessboard/routes/Game.tsx
@@ -1,7 +1,7 @@
+import { useState } from 'react';
 import { frontendUrl } from '@/config';
 import { useNavigate } from '@tanstack/react-router';
-import { Button, Flex, Typography } from 'antd';
-import { useState } from 'react';
+import { Button, Flex, Modal, Typography } from 'antd';
 import { startGame } from '../api/game';
 import { Chessboard } from '../components/Chessboard';
 import { PlayerInfo } from '../components/PlayerInfo';
@@ -18,6 +18,20 @@ export const Game = ({ useParams, useSearch }: Props) => {
     const [isMyTurn, setIsMyTurn] = useState(boardOrientation === 'white');
     const gameStarted = !!gameId && !!boardOrientation;
     const opponentBoardOrientation = boardOrientation === 'white' ? 'black' : 'white';
+    const [gameEnded, setGameEnded] = useState(false);
+
+    const gameIsEnded = () => {
+        setGameEnded(true);
+    };
+
+    const resetGame = () => {
+        setGameEnded(false);
+    };
+
+    const setUpNewGame = () => {
+        resetGame();
+        navigate({ to: '/game' });
+    };
 
     return (
         <Flex wrap="wrap">
@@ -53,10 +67,33 @@ export const Game = ({ useParams, useSearch }: Props) => {
                         {isMyTurn ? 'È il tuo turno' : "È il turno dell'avversario"}
                     </Typography.Title>
                     <PlayerInfo color={opponentBoardOrientation} myTurn={!isMyTurn} opponent />
-                    <Chessboard gameId={gameId} boardOrientation={boardOrientation} setIsMyTurn={setIsMyTurn} />
+                    <Chessboard
+                        gameId={gameId}
+                        boardOrientation={boardOrientation}
+                        setIsMyTurn={setIsMyTurn}
+                        gameIsEnded={gameIsEnded}
+                    />
                     <PlayerInfo color={boardOrientation} myTurn={isMyTurn} />
                 </Flex>
             )}
+
+            {/* Modal to show when game ends */}
+            <Modal
+                title="La partita è terminata"
+                open={gameEnded}
+                centered
+                footer={[
+                    <Button key="newGame" onClick={() => setUpNewGame()}>
+                        Nuova partita
+                    </Button>,
+                    <Button key="backToHome" onClick={() => navigate({ to: '/' })}>
+                        Torna alla home
+                    </Button>,
+                ]}
+                onCancel={() => setUpNewGame()}
+            >
+                <p>Speriamo che tu ti sia divertito</p>
+            </Modal>
         </Flex>
     );
 };
