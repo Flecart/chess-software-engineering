3d4201fe7d8e0a2bb6978d2e649d0be23030ae00
jantizio <emanuele.pischetola@studio.unibo.it>
2023-11-06 10:18:08 +0100

    feat(front): turn view

--- code/frontend/src/features/chessboard/routes/Game.tsx
+++ code/frontend/src/features/chessboard/routes/Game.tsx
@@ -1,9 +1,10 @@
 import { frontendUrl } from '@/config';
-import { UserOutlined } from '@ant-design/icons';
 import { useNavigate } from '@tanstack/react-router';
-import { Avatar, Button, Flex, Typography } from 'antd';
+import { Button, Flex, Typography } from 'antd';
+import { useState } from 'react';
 import { startGame } from '../api/game';
 import { Chessboard } from '../components/Chessboard';
+import { PlayerInfo } from '../components/PlayerInfo';
 
 type Props = {
     useParams: () => { gameId?: string };
@@ -14,52 +15,49 @@ export const Game = ({ useParams, useSearch }: Props) => {
     const { gameId } = useParams();
     const { boardOrientation } = useSearch();
     const navigate = useNavigate({ from: '/game' });
+    const [isMyTurn, setIsMyTurn] = useState(boardOrientation === 'white');
     const gameStarted = !!gameId && !!boardOrientation;
     const opponentBoardOrientation = boardOrientation === 'white' ? 'black' : 'white';
+    console.log('Game rendered');
 
     return (
-        <>
-            <h1>Game started: {String(gameStarted)}</h1>
-            <Button
-                disabled={gameStarted}
-                onClick={async () => {
-                    const res = await startGame();
-                    navigate({
-                        to: '/game/$gameId',
-                        params: { gameId: res['game-id'] },
-                        search: { boardOrientation: 'white' },
-                    });
-                }}
-            >
-                Start Game
-            </Button>
-
-            {gameStarted && (
-                <Typography.Paragraph
-                    copyable={{ text: `${frontendUrl}/game/${gameId}?boardOrientation=${opponentBoardOrientation}` }}
+        <Flex wrap="wrap">
+            <section style={{ width: '25%' }}>
+                <h1>Game started: {String(gameStarted)}</h1>
+                <Button
+                    disabled={gameStarted}
+                    onClick={async () => {
+                        const res = await startGame();
+                        navigate({
+                            to: '/game/$gameId',
+                            params: { gameId: res['game-id'] },
+                            search: { boardOrientation: 'white' },
+                        });
+                    }}
                 >
-                    Copia il link per invitare qualcuno
-                </Typography.Paragraph>
-            )}
+                    Start Game
+                </Button>
+
+                {gameStarted && (
+                    <Typography.Paragraph
+                        copyable={{
+                            text: `${frontendUrl}/game/${gameId}?boardOrientation=${opponentBoardOrientation}`,
+                        }}
+                    >
+                        Copia il link per invitare qualcuno
+                    </Typography.Paragraph>
+                )}
+            </section>
             {gameStarted && (
-                <Flex vertical gap="small" style={{ margin: '0 auto', width: 'fit-content' }}>
-                    <Flex align="center" gap="small" style={{ flexDirection: 'row-reverse' }}>
-                        <Avatar shape="square" icon={<UserOutlined />} />
-                        <p>{boardOrientation === 'black' ? 'Bianco' : 'Nero'}</p>
-                        {/* space filler */}
-                        <div style={{ width: '100%' }}></div>
-                        <div style={{ border: '1px solid', borderRadius: '3px', padding: '3px' }}>Timer</div>
-                    </Flex>
-                    <Chessboard gameId={gameId} boardOrientation={boardOrientation} />
-                    <Flex align="center" gap="small">
-                        <Avatar shape="square" icon={<UserOutlined />} />
-                        <p>{boardOrientation === 'black' ? 'Nero' : 'Bianco'}</p>
-                        {/* space filler */}
-                        <div style={{ width: '100%' }}></div>
-                        <div style={{ border: '1px solid', borderRadius: '3px', padding: '3px' }}>Timer</div>
-                    </Flex>
+                <Flex vertical gap="small">
+                    <Typography.Title level={3} type={isMyTurn ? 'success' : 'danger'}>
+                        {isMyTurn ? 'È il tuo turno' : "È il turno dell'avversario"}
+                    </Typography.Title>
+                    <PlayerInfo color={opponentBoardOrientation} myTurn={!isMyTurn} opponent />
+                    <Chessboard gameId={gameId} boardOrientation={boardOrientation} setIsMyTurn={setIsMyTurn} />
+                    <PlayerInfo color={boardOrientation} myTurn={isMyTurn} />
                 </Flex>
             )}
-        </>
+        </Flex>
     );
 };
