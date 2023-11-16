5a2be5c66ad5b061a221ed36d758efd2d4f7369e
Filippo Nardon <filippo.nardon@studio.unibo.it>
2023-11-16 18:23:09 +0100

    fix(webhook): multiple deploy

--- infra/webhook/webhook.py
+++ infra/webhook/webhook.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 import threading
+import queue
 
 app = Flask(__name__)
 app.config["DEBUG"] = False
@@ -10,19 +11,30 @@ app.config["DEBUG"] = False
 post_script = os.getenv("POST_SCRIPT")
 webhook_token = os.getenv("WEBHOOK_TOKEN")
 
+q = queue.Queue()
 
 def deploy():
   try:
       output = subprocess.check_output(
           post_script, executable='/bin/bash', shell=True,
           stderr=subprocess.STDOUT, universal_newlines=True)
-      print("Deploy completed", flush=True)
   except subprocess.CalledProcessError as er:
       print(er.output, file=sys.stderr)
       return False, er.output
   else:
       print(output, file=sys.stderr)
       return True, output
+
+def worker():
+    while True:
+        if not q.empty():
+            item = q.get()
+            print("Deploying", flush=True)
+            item.start()
+            item.join()
+            print("Deploy completed", flush=True)
+
+
 
 
 @app.route("/", methods=["POST"])
@@ -38,10 +50,12 @@ def root():
     if event_type == 'Push Hook':
         print("Received push event", flush=True)
         t = threading.Thread(target=deploy)
-        print("Starting deploy", flush=True)
-        t.start()
+        print("Add event to queue", flush=True)
+        q.put(t)
 
     return "Ok", 200
 
 if __name__ == "__main__":
+    w = threading.Thread(target=worker)
+    w.start()
     app.run(host="0.0.0.0", port=80)
