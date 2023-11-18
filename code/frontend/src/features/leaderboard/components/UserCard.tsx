fce9a986c9e8f06bc9b2c2d4aa8ba803479de43e
jantizio <emanuele.pischetola@studio.unibo.it>
2023-11-18 11:11:58 +0100

    feat: restyle leaderboard

--- code/frontend/src/features/leaderboard/components/UserCard.tsx
+++ code/frontend/src/features/leaderboard/components/UserCard.tsx
@@ -1,52 +1,66 @@
 import { Icon } from '@/components/Icon';
-import { Avatar, Flex, Typography } from 'antd';
+import type { User } from '@/features/user';
+import { Avatar, Card, Flex, Typography } from 'antd';
+import { ArrowDownCircle, ArrowUpCircle } from 'lucide-react';
 import BronzeMedal from '/bronze_medal.png';
 import GoldMedal from '/gold_medal.png';
 import SilverMedal from '/silver_medal.png';
 
 type Props = Readonly<{
-    user: {
-        username: string;
-        elo: number;
-        avatar: string;
-        wins: number;
-        losses: number;
-        draws: number;
-    };
-    first?: boolean;
-    second?: boolean;
-    third?: boolean;
+    user: User;
+    position: number;
 }>;
 
-export const Usercard = ({ user, first, second, third }: Props) => {
-    return (
-        <Flex
-            style={{
-                padding: '20px',
-                margin: '10px',
-                backgroundColor: '#DFDFDF',
-                border: '1px solid black',
-                borderRadius: '8px',
-            }}
-            gap="10px"
-        >
-            <Flex align="center">
-                {first && <Icon icon={GoldMedal} alt="Gold Medal Icon" />}
-                {second && <Icon icon={SilverMedal} alt="Silver Medal Icon" />}
-                {third && <Icon icon={BronzeMedal} alt="Bronze Medal Icon" />}
-                <Avatar src={user.avatar} alt="avatar" size={100} />
-            </Flex>
+const bigText: React.CSSProperties = { fontSize: '16px' };
+const columnStyle: React.CSSProperties = { width: '33%' };
 
-            <Flex justify="center" align="center" vertical>
-                <Typography.Text>{user.username}</Typography.Text>
-                <Typography.Text>{user.elo}</Typography.Text>
-            </Flex>
-            <div style={{ flexGrow: '1' }}></div>
-            <Flex justify="center" align="center" vertical>
-                <Typography.Text>Vittorie: {user.wins}</Typography.Text>
-                <Typography.Text>Sconfitte: {user.losses}</Typography.Text>
-                <Typography.Text>Pareggi: {user.draws}</Typography.Text>
-            </Flex>
-        </Flex>
+const { Text } = Typography;
+
+export const Usercard = ({ user, position }: Props) => {
+    return (
+        <Card hoverable bordered>
+            <Card.Meta
+                avatar={
+                    <Flex align="center">
+                        <div style={{ minWidth: '40px' }}>
+                            {position === 1 && <Icon icon={GoldMedal} alt="Gold Medal Icon" />}
+                            {position === 2 && <Icon icon={SilverMedal} alt="Silver Medal Icon" />}
+                            {position === 3 && <Icon icon={BronzeMedal} alt="Bronze Medal Icon" />}
+                            {position > 3 && (
+                                <Text strong style={{ fontSize: '20px' }}>
+                                    #{position}
+                                </Text>
+                            )}
+                        </div>
+
+                        <Avatar src={user.avatar} alt="avatar" size={100} />
+                    </Flex>
+                }
+                title={<Text style={{ fontSize: '20px' }}>{user.username}</Text>}
+                description={
+                    <Flex justify="space-between" align="center" wrap="wrap">
+                        <Text style={{ ...bigText, ...columnStyle, color: 'GrayText' }}>ELO: {user.elo}</Text>
+                        <Flex align="center" justify="center" gap="4px" style={columnStyle}>
+                            <ArrowUpCircle color="#009B77" />
+                            <Text style={bigText}>
+                                Vittorie:{' '}
+                                <Text style={bigText} strong>
+                                    {user.wins}
+                                </Text>
+                            </Text>
+                        </Flex>
+                        <Flex align="center" justify="center" gap="4px" style={columnStyle}>
+                            <ArrowDownCircle color="#a1000e" />
+                            <Text style={bigText}>
+                                Sconfitte:{' '}
+                                <Text style={bigText} strong>
+                                    {user.losses}
+                                </Text>
+                            </Text>
+                        </Flex>
+                    </Flex>
+                }
+            />
+        </Card>
     );
 };
--- code/frontend/src/features/leaderboard/routes/Leaderboard.tsx
+++ code/frontend/src/features/leaderboard/routes/Leaderboard.tsx
@@ -1,24 +1,43 @@
+import type { User } from '@/features/user';
 import { Flex, Typography } from 'antd';
 import { Usercard } from '../components/UserCard';
+
+const { Title } = Typography;
+const textHeaderStyle: React.CSSProperties = { margin: '0', marginBottom: '10px' };
 
 export const Leaderboard = () => {
     return (
         <Flex vertical gap="10px" style={{ maxWidth: '800px', margin: '0 auto' }}>
-            <Typography.Title level={1} style={{ textAlign: 'center' }}>
+            <Title level={1} style={{ textAlign: 'center' }}>
                 Classifica
-            </Typography.Title>
+            </Title>
+
+            <Flex style={{ padding: '0 5px', margin: '0 20px 20px', borderBottom: '2px solid #716A6A' }} align="center">
+                <div style={{ width: '11%' }}></div>
+                <Title level={3} style={textHeaderStyle}>
+                    Utente
+                </Title>
+                <div style={{ width: '34%' }}></div>
+                <Title level={3} style={textHeaderStyle}>
+                    Vittorie
+                </Title>
+                <div style={{ width: '15%' }}></div>
+                <Title level={3} style={textHeaderStyle}>
+                    Sconfitte
+                </Title>
+            </Flex>
+
             {mockUsers.map((user, i) => (
-                <Usercard key={user.username} user={user} first={i === 0} second={i === 1} third={i === 2} />
+                <Usercard key={user.username} user={user} position={i + 1} />
             ))}
         </Flex>
     );
 };
 
-const mockUsers = Array.from({ length: 10 }, (_, i) => ({
+const mockUsers: User[] = Array.from({ length: 10 }, (_, i) => ({
     username: `User${i + 1}`,
     elo: Math.floor(Math.random() * 2000),
     avatar: `https://api.dicebear.com/7.x/croodles/svg?seed=${i + 1}`,
     wins: Math.floor(Math.random() * 100),
     losses: Math.floor(Math.random() * 100),
-    draws: Math.floor(Math.random() * 100),
 }));
