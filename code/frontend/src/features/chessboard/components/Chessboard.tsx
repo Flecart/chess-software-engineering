8e121b433ec75d46100f658928887ac44944c437
Emanuele Pischetola <emanuele.pischetola@studio.unibo.it>
2023-11-22 23:27:28 +0100

    feat: promotion handling
    Co-authored-by: Xuanqiang Huang <xuanqiang.huang@studio.unibo.it>
    Co-authored-by: Alessandro Neri <alessandro.neri21@studio.unibo.it>

--- code/frontend/src/features/chessboard/components/Chessboard.tsx
+++ code/frontend/src/features/chessboard/components/Chessboard.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { Chessboard as ReactChessboard } from 'react-chessboard';
 import { generateFogObject, generateOldFogFen, generateStandardFen } from '../utils/fen';
+import type { Piece, Square } from 'react-chessboard/dist/chessboard/types';
 
 const startBlackFEN = 'rnbqkbnr/pppppppp/......../......../XXXXXXXX/XXXXXXXX/XXXXXXXX/XXXXXXXX';
 const startWhiteFEN = startBlackFEN.toUpperCase().split('/').reverse().join('/');
@@ -15,6 +17,7 @@ type Props = {
 
 export const Chessboard = ({ fen, boardOrientation, style, makeMove }: Props) => {
     if (!fen) fen = boardOrientation === 'white' ? startWhiteFEN : startBlackFEN;
+    const [lastMove, setLastMove] = useState<string[] | undefined>(undefined);
 
     return (
         <div
@@ -35,10 +38,34 @@ export const Chessboard = ({ fen, boardOrientation, style, makeMove }: Props) =>
                         (boardOrientation === 'white' && piece.startsWith('b'))
                     )
                         return false;
+
                     makeMove(from, to);
                     return true; //TODO: seems like it doesn't matter the return value, investigate
                 }}
+                onPromotionPieceSelect={(piece) => {
+                    if (piece && lastMove) {
+                        makeMove(`${lastMove[0]}`, `${lastMove[1]}${piece[1]?.toLowerCase()}`);
+                    }
+                    return true;
+                }}
+                onPromotionCheck={(source, target, piece) => {
+                    if (
+                        (boardOrientation === 'black' && piece.startsWith('w')) ||
+                        (boardOrientation === 'white' && piece.startsWith('b'))
+                    )
+                        return false;
+                    setLastMove([source, target]);
+                    return isPromotion(source, target, piece);
+                }}
             />
         </div>
+    );
+};
+
+const isPromotion = (sourceSquare: Square, targetSquare: Square, piece: Piece) => {
+    return (
+        ((piece === 'wP' && sourceSquare[1] === '7' && targetSquare[1] === '8') ||
+            (piece === 'bP' && sourceSquare[1] === '2' && targetSquare[1] === '1')) &&
+        Math.abs(sourceSquare.charCodeAt(0) - targetSquare.charCodeAt(0)) <= 1
     );
 };
