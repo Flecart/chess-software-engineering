c448cc252ece68dc2400cf8ee5221433fbc1c034
jantizio <emanuele.pischetola@studio.unibo.it>
2023-11-22 11:34:13 +0100

    fix: missing imports

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { RouterProvider } from '@tanstack/react-router';
 import { ConfigProvider as AntdConfigProvider, theme } from 'antd';
 import itIT from 'antd/lib/locale/it_IT';
 import { queryClient } from '@/lib/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const App = () => {
     return (
