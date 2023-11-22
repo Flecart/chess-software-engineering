2a05a1caa88e154981cb14a00dd564742a1d0c7c
Giovanni Spadaccini <giospadaccini74@gmail.com>
2023-11-22 17:04:52 +0100

    fix(frontend): piccole chicce

--- code/frontend/src/features/leaderboard/components/UserCard.tsx
+++ code/frontend/src/features/leaderboard/components/UserCard.tsx
@@ -5,6 +5,7 @@ import { ArrowDownCircle, ArrowUpCircle } from 'lucide-react';
 import BronzeMedal from '/bronze_medal.png';
 import GoldMedal from '/gold_medal.png';
 import SilverMedal from '/silver_medal.png';
+import { Link } from '@tanstack/react-router';
 
 type Props = Readonly<{
     user: User;
@@ -18,49 +19,51 @@ const { Text } = Typography;
 
 export const Usercard = ({ user, position }: Props) => {
     return (
-        <Card hoverable bordered>
-            <Card.Meta
-                avatar={
-                    <Flex align="center">
-                        <div style={{ minWidth: '40px' }}>
-                            {position === 1 && <Icon icon={GoldMedal} alt="Gold Medal Icon" />}
-                            {position === 2 && <Icon icon={SilverMedal} alt="Silver Medal Icon" />}
-                            {position === 3 && <Icon icon={BronzeMedal} alt="Bronze Medal Icon" />}
-                            {position > 3 && (
-                                <Text strong style={{ fontSize: '20px' }}>
-                                    #{position}
-                                </Text>
-                            )}
-                        </div>
+        <Link to="/profile/$username" params={{ username: user.username }}>
+            <Card hoverable bordered>
+                <Card.Meta
+                    avatar={
+                        <Flex align="center">
+                            <div style={{ minWidth: '40px' }}>
+                                {position === 1 && <Icon icon={GoldMedal} alt="Gold Medal Icon" />}
+                                {position === 2 && <Icon icon={SilverMedal} alt="Silver Medal Icon" />}
+                                {position === 3 && <Icon icon={BronzeMedal} alt="Bronze Medal Icon" />}
+                                {position > 3 && (
+                                    <Text strong style={{ fontSize: '20px' }}>
+                                        #{position}
+                                    </Text>
+                                )}
+                            </div>
 
-                        <Avatar src={user.avatar} alt="avatar" size={100} />
-                    </Flex>
-                }
-                title={<Text style={{ fontSize: '20px' }}>{user.username}</Text>}
-                description={
-                    <Flex justify="space-between" align="center" wrap="wrap">
-                        <Text style={{ ...bigText, ...columnStyle, color: 'GrayText' }}>ELO: {user.elo}</Text>
-                        <Flex align="center" justify="center" gap="4px" style={columnStyle}>
-                            <ArrowUpCircle color="#009B77" />
-                            <Text style={bigText}>
-                                Vittorie:{' '}
-                                <Text style={bigText} strong>
-                                    {user.wins}
-                                </Text>
-                            </Text>
+                            <Avatar src={user.avatar} alt="avatar" size={100} />
                         </Flex>
-                        <Flex align="center" justify="center" gap="4px" style={columnStyle}>
-                            <ArrowDownCircle color="#a1000e" />
-                            <Text style={bigText}>
-                                Sconfitte:{' '}
-                                <Text style={bigText} strong>
-                                    {user.losses}
+                    }
+                    title={<Text style={{ fontSize: '20px' }}>{user.username}</Text>}
+                    description={
+                        <Flex justify="space-between" align="center" wrap="wrap">
+                            <Text style={{ ...bigText, ...columnStyle, color: 'GrayText' }}>ELO: {user.elo}</Text>
+                            <Flex align="center" justify="center" gap="4px" style={columnStyle}>
+                                <ArrowUpCircle color="#009B77" />
+                                <Text style={bigText}>
+                                    Vittorie:{' '}
+                                    <Text style={bigText} strong>
+                                        {user.wins}
+                                    </Text>
                                 </Text>
-                            </Text>
+                            </Flex>
+                            <Flex align="center" justify="center" gap="4px" style={columnStyle}>
+                                <ArrowDownCircle color="#a1000e" />
+                                <Text style={bigText}>
+                                    Sconfitte:{' '}
+                                    <Text style={bigText} strong>
+                                        {user.losses}
+                                    </Text>
+                                </Text>
+                            </Flex>
                         </Flex>
-                    </Flex>
-                }
-            />
-        </Card>
+                    }
+                />
+            </Card>
+        </Link>
     );
 };
