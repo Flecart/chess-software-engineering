394957211bdbe5eb00ab489dd89960b20ba894a8
Filippo Nardon <filippo.nardon@studio.unibo.it>
2023-11-22 14:30:49 +0000

    fix(frontend): wrong behaviour on websocket url declaration

--- code/frontend/src/config/index.ts
+++ code/frontend/src/config/index.ts
@@ -1,5 +1,5 @@
 export const backendUrl = (import.meta.env.VITE_BACKEND_URL as string) ?? 'http://localhost:8000';
 export const frontendUrl = (import.meta.env.VITE_FRONTEND_URL as string) ?? 'http://localhost:5173';
+export const wsUrl = (import.meta.env.VITE_WEBSOCKET_URL as string) ?? 'ws://localhost:8000';
 
-export const wsUrl = backendUrl.replace(/^http/, 'ws');
 export const apiBaseUrl = '/api/v1';
