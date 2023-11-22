9ad99642a5eb0681792416332463aa873bbfb87a
Giovanni Spadaccini <giospadaccini74@gmail.com>
2023-11-22 16:53:41 +0100

    feat(backend): leaderbord and info games and info user

--- code/frontend/src/features/leaderboard/api/leaderboard.ts
+++ code/frontend/src/features/leaderboard/api/leaderboard.ts
@@ -1,16 +1,7 @@
 import type { User } from '@/features/user';
-// import { axios } from '@/lib/axios';
+import { axios } from '@/lib/axios';
 
 export async function getLeaderBoard(): Promise<User[]> {
-    // TODO: do real api call
-    // const response = await axios.get('/leaderboard');
-    // return response.data;
-
-    return Array.from({ length: 10 }, (_, i) => ({
-        username: `User${i + 1}`,
-        elo: Math.floor(Math.random() * 2000),
-        avatar: `https://api.dicebear.com/7.x/croodles/svg?seed=${i + 1}`,
-        wins: Math.floor(Math.random() * 100),
-        losses: Math.floor(Math.random() * 100),
-    }));
+    const response = await axios.get('/api/v1/user/leaderboard');
+    return response.data;
 }
