c763ed14198acbfff68107aa11614874d02c40f9
Emanuele Pischetola <emanuele.pischetola@studio.unibo.it>
2023-11-25 10:10:57 +0100

    build: changed env variable
    ho cambiato le variabili d'ambiente per avere meno ripetizione sull'host

--- code/frontend/src/config/index.ts
+++ code/frontend/src/config/index.ts
@@ -1,5 +1,10 @@
-export const backendUrl = (import.meta.env.VITE_BACKEND_URL as string) ?? 'http://localhost:8000';
+export const protocol = (import.meta.env.VITE_PROTOCOL as string) ?? 'http';
+export const wsProtocol = (import.meta.env.VITE_WS_PROTOCOL as string) ?? 'ws';
+export const host = (import.meta.env.VITE_HOST as string) ?? 'localhost:8000';
+
+export const backendUrl = `${protocol}://${host}`;
+export const wsUrl = `${wsProtocol}://${host}`;
+
 export const frontendUrl = (import.meta.env.VITE_FRONTEND_URL as string) ?? 'http://localhost:5173';
-export const wsUrl = (import.meta.env.VITE_WEBSOCKET_URL as string) ?? 'ws://localhost:8000';
 
 export const apiBaseUrl = '/api/v1';
