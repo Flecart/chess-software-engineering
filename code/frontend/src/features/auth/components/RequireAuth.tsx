f9e09d00fe944102094530e64b6acc056b469455
Emanuele Pischetola <emanuele.pischetola@studio.unibo.it>
2023-11-25 12:12:15 +0100

    fix: navigate bug

--- code/frontend/src/features/auth/components/RequireAuth.tsx
+++ code/frontend/src/features/auth/components/RequireAuth.tsx
@@ -1,8 +1,18 @@
-import { Navigate, Outlet } from '@tanstack/react-router';
-import { useAuth } from '../hooks/useAuth';
+import { Outlet, useNavigate } from '@tanstack/react-router';
+import { useEffect } from 'react';
+import { AuthSwitch } from '../components/AuthSwitch';
 
-export const RequireAuth = () => {
-    const { isAuth } = useAuth();
+export const RequireAuth = AuthSwitch({
+    AuthComponent: () => <Outlet />,
+    UnauthComponent: () => {
+        // WORKAROUND: using the Navigate component directly, updates the URL but does not render the component
+        // https://github.com/TanStack/router/issues/801
+        // using the useNavigate hook, updates the URL and renders the component
 
-    return isAuth ? <Outlet /> : <Navigate to="/login" replace />;
-};
+        const navigate = useNavigate();
+        useEffect(() => {
+            navigate({ to: '/login', replace: true });
+        });
+        return <></>;
+    },
+});
--- code/frontend/src/features/user/routes/ProfileRedirect.tsx
+++ code/frontend/src/features/user/routes/ProfileRedirect.tsx
@@ -1,8 +1,16 @@
 import { useAuth } from '@/features/auth';
-import { Navigate } from '@tanstack/react-router';
+import { useNavigate } from '@tanstack/react-router';
+import { useEffect } from 'react';
 
 export const ProfileRedirect = () => {
     const { username } = useAuth();
 
-    return <Navigate to="/profile/$username" params={{ username }} replace />;
+    // WORKAROUND: using the Navigate component directly, updates the URL but does not render the component
+    // https://github.com/TanStack/router/issues/801
+    // using the useNavigate hook, updates the URL and renders the component
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate({ to: '/profile/$username', params: { username }, replace: true });
+    });
+    return <></>;
 };
