d9394a6843379fc3332ae05dce8b932c5a586c91
Emanuele Pischetola <emanuele.pischetola@studio.unibo.it>
2023-11-25 11:27:27 +0100

    style: improved auth landing aesthetics

--- code/frontend/src/features/misc/routes/AuthLanding.tsx
+++ code/frontend/src/features/misc/routes/AuthLanding.tsx
@@ -23,33 +23,44 @@ export const AuthLanding = () => {
                     {username}
                 </Link>
             </Flex>
-            <Flex justify="center" vertical style={{ maxWidth: '600px', margin: '0 auto' }}>
-                <Typography.Title
-                    level={2}
-                    style={{
-                        padding: '15px',
-                        border: '1px solid #ddd',
-                        borderRadius: '5px',
-                        backgroundColor: '#ededed',
-                        boxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
-                    }}
-                >
-                    Le tue partite recenti
-                </Typography.Title>
-                <Flex
-                    gap="small"
-                    align="center"
-                    justify="center"
-                    style={{
-                        padding: '10px',
-                        border: '1px solid #ddd',
-                        borderRadius: '5px',
-                        backgroundColor: '#fff',
-                    }}
-                >
-                    {games.map((game) => (
-                        <GameCard key={game.id} game={game} size="small" />
-                    ))}
+            <Flex justify="space-around" align="center">
+                <Flex justify="center" vertical style={{ maxWidth: '600px' }}>
+                    <Typography.Title
+                        level={2}
+                        style={{
+                            padding: '15px',
+                            border: '1px solid #ddd',
+                            borderRadius: '5px',
+                            backgroundColor: '#ededed',
+                            boxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
+                        }}
+                    >
+                        Le tue partite recenti
+                    </Typography.Title>
+                    <Flex
+                        gap="small"
+                        align="center"
+                        justify="center"
+                        vertical
+                        style={{
+                            padding: '10px',
+                            border: '1px solid #ddd',
+                            borderRadius: '5px',
+                            backgroundColor: '#fff',
+                        }}
+                    >
+                        {games.map((game) => (
+                            <GameCard key={game.id} game={game} size="small" />
+                        ))}
+                    </Flex>
+                </Flex>
+                <Flex gap="large" vertical style={{ maxWidth: '540px' }}>
+                    <Typography.Title>Federazione Italiana Scacchisti Eterodossi - Check Mates</Typography.Title>
+                    <Typography.Paragraph style={{ textAlign: 'justify' }}>
+                        Dark chess è una variante degli scacchi con informazione incompleta, simile al Kriegspiel. È
+                        stato inventato da Jens Bæk Nielsen e Torben Osted nel 1989. Un giocatore non vede l'intera
+                        scacchiera - solo i propri pezzi e le caselle in cui può legalmente muoversi.
+                    </Typography.Paragraph>
                 </Flex>
             </Flex>
             <Flex justify="center" gap={100} wrap="wrap" style={{ width: '100%', marginTop: '120px' }}>
