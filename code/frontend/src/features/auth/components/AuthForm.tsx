c26482f322102d8e1b47f9990646f2580b49b67f
Emanuele Pischetola <emanuele.pischetola@studio.unibo.it>
2023-11-26 10:39:45 +0100

    style: removed useless fragment

--- code/frontend/src/features/auth/components/AuthForm.tsx
+++ code/frontend/src/features/auth/components/AuthForm.tsx
@@ -13,30 +13,28 @@ export const AuthForm = ({ username, password, execAction }: Props) => {
     const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => (password.value = e.target.value);
 
     return (
-        <>
-            <Form name="basic-auth" wrapperCol={{ span: 16 }} style={{ maxWidth: 600 }} onFinish={execAction}>
-                <Form.Item<AuthPayload>
-                    label="Username"
-                    name="username"
-                    rules={[{ required: true, message: 'Inserisci il nome utente' }]}
-                >
-                    <Input onChange={onChangeUsername} />
-                </Form.Item>
+        <Form name="basic-auth" wrapperCol={{ span: 16 }} style={{ maxWidth: 600 }} onFinish={execAction}>
+            <Form.Item<AuthPayload>
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Inserisci il nome utente' }]}
+            >
+                <Input onChange={onChangeUsername} />
+            </Form.Item>
 
-                <Form.Item<AuthPayload>
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Inserisci la password' }]}
-                >
-                    <Input.Password onChange={onChangePassword} />
-                </Form.Item>
+            <Form.Item<AuthPayload>
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Inserisci la password' }]}
+            >
+                <Input.Password onChange={onChangePassword} />
+            </Form.Item>
 
-                <Form.Item wrapperCol={{ span: 16 }}>
-                    <Button type="primary" htmlType="submit">
-                        Conferma
-                    </Button>
-                </Form.Item>
-            </Form>
-        </>
+            <Form.Item wrapperCol={{ span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Conferma
+                </Button>
+            </Form.Item>
+        </Form>
     );
 };
