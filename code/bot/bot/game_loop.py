705e6514a49557c7b9cf34198b758b9b5674c084
Emanuele Pischetola <emanuele.pischetola@studio.unibo.it>
2023-12-05 20:04:43 +0100

    feat: end game message, relative time left

--- code/bot/bot/game_loop.py
+++ code/bot/bot/game_loop.py
@@ -1,10 +1,18 @@
 import asyncio
-from datetime import datetime, timedelta
+from datetime import timedelta
 from telebot.async_telebot import AsyncTeleBot
-from bot.api_utils import create_game, delete_game, get_possible_moves, make_move, wait_opponent_move
-from bot.ws_utils import GameStatus, WebSocketWrapper
+from bot.api_utils import (
+    create_game,
+    delete_game,
+    get_possible_moves,
+    make_move,
+    wait_opponent_move,
+)
+from bot.utils import pretty_print_time
+from bot.ws_utils import WebSocketWrapper
 from bot.src.ballot_box_collection import BallotBoxCollection
 from bot.display_board import custom_fen_to_svg
+
 
 async def game_loop(time: int, chatid: int, bot: AsyncTeleBot):
     """
@@ -12,22 +20,21 @@ async def game_loop(time: int, chatid: int, bot: AsyncTeleBot):
     chatid := id chat dov'è stata avviata la partita
     bot := l'istanza del bot in esecuzione
     """
-    token, game_id = create_game(chatid)
+    color = "white"
+    token, game_id = create_game(chatid, color)
     ws = WebSocketWrapper(game_id, token)
-    possible_moves = []
-    max_voted = ''
+    max_voted = ""
     gamestatus = await ws.connect()
 
     while True:
-        # stampa mossa avversaria -> avvia voto -> salva & invia voto -> 
+        # stampa mossa avversaria -> avvia voto -> salva & invia voto
+        await bot.send_photo(chatid, custom_fen_to_svg(gamestatus["view"]))
 
-        # get bot move
-
-        #await bot.send_message(chatid, gamestatus['view'] )
-        await bot.send_photo(chatid, custom_fen_to_svg(gamestatus['view'])) 
-
-        if gamestatus['ended']:
-            await bot.send_message(chatid,'partita finita!!!')
+        if gamestatus["ended"]:
+            result_message = (
+                "avete vinto!" if gamestatus["turn"] != color else "avete perso!"
+            )
+            await bot.send_message(chatid, f"partita finita!!! {result_message}")
             ws.close()
             break
 
@@ -35,41 +42,31 @@ async def game_loop(time: int, chatid: int, bot: AsyncTeleBot):
         ws.close()
 
         # start time to vote
-        current_time = datetime.now()
-        end_vote_time = current_time + timedelta(seconds=time)
+
+        remaining_time = timedelta(seconds=time)
 
         await bot.send_message(
-            chatid,
-            f'le mosse possibili sono { ", ".join(possible_moves)}'
+            chatid, f'le mosse possibili sono { ", ".join(possible_moves)}'
         )
         await bot.send_message(
             chatid,
-            f'vota la mossa! Hai tempo fino alle {end_vote_time.strftime("%H:%M:%S")}',
+            f"vota la mossa! Hai tempo rimanente: {pretty_print_time(remaining_time)}",
         )
 
         await asyncio.sleep(time)
 
         # chiudere votazione e inviare la mossa scelta
 
-
         max_voted = BallotBoxCollection().mostVoted(chatid)
         BallotBoxCollection().reset_box(chatid)
 
         await ws.connect()
 
-        gamestatus = await make_move(ws,max_voted)
+        gamestatus = await make_move(ws, max_voted)
 
+        await bot.send_photo(chatid, custom_fen_to_svg(gamestatus["view"]))
 
-        """
-        await bot.send_message(
-            chatid,
-            f'ecco la board aggiornata {gamestatus["view"] }'
-        )
-        """
-        await bot.send_photo(chatid, custom_fen_to_svg(gamestatus['view'])) 
-
+        # get bot move
         gamestatus = await wait_opponent_move(ws)
 
-
     delete_game(chatid)
-
--- code/bot/bot/utils.py
+++ code/bot/bot/utils.py
@@ -6,3 +6,18 @@ def get_user_and_chat_from_message(message: types.Message) -> tuple[int, int]:
     chat_id = message.chat.id
     return (chat_id, user_id)
 
+
+def pretty_print_time(td):
+    parts = []
+    if td.days > 0:
+        parts.append(f"{td.days} giorni")
+    hours = td.seconds // 3600
+    if hours > 0:
+        parts.append(f"{hours} ore")
+    minutes = (td.seconds % 3600) // 60
+    if minutes > 0:
+        parts.append(f"{minutes} minuti")
+    seconds = td.seconds % 60
+    if seconds > 0:
+        parts.append(f"{seconds} secondi")
+    return " ".join(parts)
