ff910773707a6f9351cc5d6cc1c9c375dac41c75
Emanuele Pischetola <emanuele.pischetola@studio.unibo.it>
2023-12-06 15:34:26 +0100

    refactor: deleted old files

--- code/backend/backend/engine/README.md
+++
@@ -1,3 +0,0 @@
-# The Engine
-
-L'Engine per Dark chess qui presente è adattato da https://github.com/AHAPX/dark-chess
--- code/backend/backend/routes/__init__.py
+++ code/backend/backend/routes/__init__.py
@@ -3,44 +3,33 @@ from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 from backend.routes.user.user import create_user_routes
-from backend.routes.darkboard.darkboardgame import create_game_routes as create_darkboard_routes
+from backend.routes.darkboard.darkboardgame import (
+    create_game_routes as create_darkboard_routes,
+)
 
 from .game.game import create_game_routes
 from .exception import install_exception_handler
-from backend.database.database import engine, Base
 
-# TODO(ang): delete these after frontend and telegram is updated
-from .old.game import  create_game_routes as oldgameroute
-from .old.game_old import create_game_routes as oldroute
-from .old.user import create_user_routes as olduser
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # nonostante con allow_origin sembri accessibile a tutti, non sarà un servizio esposto su internet,
     # quindi stiamo permettendo la comunicazione solamente con i servizi che si trovano come descritti dal docker compose.
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-
-logging.getLogger('main').info('Creating routes')
+logging.getLogger("main").info("Creating routes")
 """ adding routes  """
 
 install_exception_handler(app)
 
-create_game_routes(app, prefix='/api/v1')
-create_user_routes(app, prefix='/api/v1')
-create_darkboard_routes(app, prefix='/api/v1')
+create_game_routes(app, prefix="/api/v1")
+create_user_routes(app, prefix="/api/v1")
+create_darkboard_routes(app, prefix="/api/v1")
 
-# TODO OLD TO DELETE AS SOON AS THE PORTING IS DONE
-oldroute(app)
-oldgameroute(app,prefix='/dev')
-olduser(app,prefix='/dev')
-
-
-logging.getLogger('main').info('Createe routes')
-
+logging.getLogger("main").info("Createe routes")
