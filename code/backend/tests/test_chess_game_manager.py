6af8af72a72a4cb964c0c6a7335c5fa382c77ea0
Emanuele Pischetola <emanuele.pischetola@studio.unibo.it>
2023-12-07 11:47:08 +0100

    test: game manager tests
    Co-authored-by: Xuanqiang Huang <xuanqiang.huang@studio.unibo.it>

--- code/backend/tests/test_chess_game_manager.py
+++ code/backend/tests/test_chess_game_manager.py
@@ -1,47 +1,49 @@
 import unittest
 from unittest.mock import patch, MagicMock
+
 from backend.game.v1_chess_game_manager import ChessGameManager
 from backend.game.v1_chess_game import ChessGame, Color, CreateGameRequest
 
 
-@unittest.skip("Skip for now")
 class TestChessGameManager(unittest.TestCase):
     def setUp(self):
         self.manager = ChessGameManager()
+        self.game_request = CreateGameRequest(
+            type="dark_chess", against_bot=False, time=10
+        )
+
+    def tearDown(self):
+        self.manager._instance = None
 
     def test_singleton(self):
         manager2 = ChessGameManager()
         self.assertEqual(self.manager, manager2)
 
     def test_create_new_game(self):
-        game_request = CreateGameRequest(type="dark_chess", against_bot=False, time=10)
-        game_id = self.manager.create_new_game(game_request)
+        game_id = self.manager.create_new_game(self.game_request)
         self.assertIsInstance(self.manager.get_game(game_id), ChessGame)
 
     def test_join(self):
-        game_request = CreateGameRequest(type="dark_chess", against_bot=False, time=10)
-        game_id = self.manager.create_new_game(game_request)
-        self.manager.join(game_id, 1, Color.WHITE)
+        game_id = self.manager.create_new_game(self.game_request)
+        self.manager.join(game_id, 0, Color.WHITE)
         with self.assertRaises(ValueError):
-            self.manager.join(game_id, 1, Color.WHITE)
-        self.manager.join(game_id, 2, Color.BLACK)
+            self.manager.join(game_id, 0, Color.WHITE)
+        self.manager.join(game_id, 1, Color.BLACK)
         with self.assertRaises(ValueError):
-            self.manager.join(game_id, 2, Color.BLACK)
+            self.manager.join(game_id, 1, Color.BLACK)
 
     @patch.object(ChessGame, "move")
     def test_move(self, mock_move):
-        game_request = CreateGameRequest(type="dark_chess", against_bot=False, time=10)
-        game_id = self.manager.create_new_game(game_request)
-        self.manager.join(game_id, 1, Color.WHITE)
-        self.manager.join(game_id, 2, Color.BLACK)
-        self.manager.move(game_id, 1, "e2e4")
+        game_id = self.manager.create_new_game(self.game_request)
+        self.manager.join(game_id, 2, Color.WHITE)
+        self.manager.join(game_id, 3, Color.BLACK)
+        self.manager.move(game_id, 2, "e2e4")
         mock_move.assert_called_once_with("e2e4")
 
     def test_get_view(self):
-        game_request = CreateGameRequest(type="dark_chess", against_bot=False, time=10)
-        game_id = self.manager.create_new_game(game_request)
-        self.manager.join(game_id, 1, Color.WHITE)
-        self.manager.join(game_id, 2, Color.BLACK)
+        game_id = self.manager.create_new_game(self.game_request)
+        self.manager.join(game_id, 4, Color.WHITE)
+        self.manager.join(game_id, 5, Color.BLACK)
         self.assertIsInstance(self.manager.get_view(game_id, Color.WHITE), str)
         self.assertIsInstance(self.manager.get_view(game_id, Color.BLACK), str)
 
